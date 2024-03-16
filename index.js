715646150540ae2ec4727391ba59697814252255
Elivaldo dos Santos <150296445+elivaldoquio@users.noreply.github.com>
2024-03-16 18:25:15 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 function solucao(lista) {
-   
+
   let totalAcumulado = 0;
   for (let i = 0; i < lista.length; i++) {
-  totalAcumulado = totalAcumulado + lista[i];
-    }
-    console.log(totalAcumulado)
+    totalAcumulado = totalAcumulado + lista[i];
+  }
 
+  console.log(totalAcumulado);
 }
+const lista = [1, 2, 3, 4];
+solucao(lista)
